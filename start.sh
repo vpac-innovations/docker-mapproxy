b3354ac87ee2dc0c5bda1e08fc8c98bdcbcce3e5
Jin Park <forjin@vpac-innovations.com.au>
2016-05-24 17:17:57 +1000

    remove pid and forcely add a group

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ USER_ID=`ls -lahn / | grep mapproxy | awk '{print $3}'`
 GROUP_ID=`ls -lahn / | grep mapproxy | awk '{print $4}'`
 USER_NAME=`ls -lah / | grep mapproxy | awk '{print $3}'`
 
-groupadd -g $GROUP_ID mapproxy
+groupadd -f -g $GROUP_ID mapproxy
 useradd --shell /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME
 
 # Create a default mapproxy config is one does not exist in /mapproxy
@@ -13,6 +13,7 @@ then
 fi
 cd /mapproxy
 cp /mapproxy.yaml ./mapproxy.yaml
-su $USER_NAME -c "mapproxy-util create -t wsgi-app -f mapproxy.yaml /mapproxy/app.py"
+rm -fv /tmp/mapproxy.pid
+su $USER_NAME -c "mapproxy-util create --force -t wsgi-app -f mapproxy.yaml /mapproxy/app.py"
 su $USER_NAME -c "uwsgi --protocol=http --ini /uwsgi.conf"
 #su $USER_NAME -c "/venv/bin/mapproxy-util serve-develop -b 0.0.0.0:8080 mapproxy.yaml"
